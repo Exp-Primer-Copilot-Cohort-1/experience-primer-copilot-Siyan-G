46201e0568087c66572470686b9fc62c88ab6a95
Siyan <141314561+Siyan-G@users.noreply.github.com>
2024-05-14 12:38:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    const skills = [
+        {
+            name: "Javascript",
+            level: 80
+        },
+        {
+            name: "HTML",
+            level: 90
+        },
+        {
+            name: "CSS",
+            level: 80
+        }
+    ];
+
+    return skills;
+}
